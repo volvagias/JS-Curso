8de0a6088f657fff411ca5fef79014f5680ee7f5
Leonel <leonel.bruzzese@gmail.com>
2024-08-07 10:00:51 -0300

    terminando video de arrow function

--- 10 - Arrow Functions/script.js
+++ 10 - Arrow Functions/script.js
@@ -16,5 +16,30 @@ const saludar = () => console.log(`Hola`);
 saludar();
 */
 
+// Importante: las Arrow Functions traen un return implícito con =>.
+
+// Cuando hay más de una instruccion las Arrow Functions deben llevar las llaves {}.
+const funcionDeVariasLineas = () => {
+    console.log("Uno");
+    console.log("Dos");
+}
+funcionDeVariasLineas();
+
+// Pasando un forEach a una Arrow Function:
+const numeros = [1, 2, 3, 4, 5];
+
+numeros.forEach(function(element, index) {   // sin Arrow Function
+    console.log(`El elemento ${el} está en la posición ${index}.`)
+})
+
+// con Arrow Function (puedo omitir las llaves al ser solo una instrucción).
+numeros.forEach((element, index) => console.log(`El elemento ${el} está en la posición ${index}.`));
+
+// Con más de una instrucción lleva llaves.
+numeros.forEach((element, index) => {
+    console.log(`El elemento ${el} está en la posición ${index}.`)
+    console.log("Hola");
+}); 
+
 
 
