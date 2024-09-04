bfd4a607e4e86954e4407858d2ceb33198883394
Leonel <leonel.bruzzese@gmail.com>
2024-09-04 13:58:15 -0300

    Ejercicios 4 empezando.

--- 22 - Ejercicios 3/script.js
+++ 22 - Ejercicios 3/script.js
@@ -9,9 +9,10 @@ obtenerNumero();
 
 // 10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
 
-/*
-function esCapicua(numero = undefined) {
-    if(numero === undefined) return console.error("Debe ingresar un número");
+
+function esCapicua(numero = 0) {
+    if(!numero) return console.error("Debe ingresar un número");
+    if(typeof numero !== "number") return console.error (`El valor "${numero}" ingresado NO es un número.`);
 
     numero = numero.toString();
 
@@ -23,7 +24,7 @@ function esCapicua(numero = undefined) {
     if(izqDer === derIzq) return console.log(true);
 }
 esCapicua(505);
-*/
+
 
 // 11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
-
+// no lo voy a hacer.
---
+++ 23 - Ejercicios 4/script.js
@@ -0,0 +1,25 @@
+// 12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
+
+function esPrimo(numero = undefined) {
+    if(numero === undefined) return console.error("Debe ingresar un número");
+    if(typeof numero !== "number") return console.error(`El valor "${numero}" ingresado NO es un número.`);
+    if(numero === 0 | numero === 1) return console.error("El número no puede ser 0 o 1.");
+    if(Math.sign(numero) === -1) return console.error("El número no puede ser negativo.");
+
+    let divisible = false;
+
+    for(let i = 2; i < numero; i++) {
+        if(numero % i === 0) {
+            divisible = true;
+            break;
+        }
+    }
+
+    return(divisible) 
+        ? console.log(`El número ${numero} NO es primo`)
+        : console.log(`El número ${numero} SÍ es primo.`);
+}
+esPrimo(3);
+
+// 13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
+// 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
