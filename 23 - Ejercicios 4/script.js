03f2f108e559d999141d3c48c812b1c880486be0
Leonel <leonel.bruzzese@gmail.com>
2024-09-09 13:11:49 -0300

    Ejercicios 4 terminado.

--- 23 - Ejercicios 4/script.js
+++ 23 - Ejercicios 4/script.js
@@ -22,4 +22,23 @@ function esPrimo(numero = undefined) {
 esPrimo(3);
 
 // 13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
+
+function parImpar(numero = undefined) {
+    if(numero === undefined) return console.error("Debe ingresar un número");
+    if(typeof numero !== "number") return console.error(`El valor "${numero}" ingresado NO es un número.`);
+    if(numero === 0) return console.error("El número no puede ser un 0.")
+    if(Math.sign(numero) === -1) return console.error("El número no puede ser negativo.");
+
+    // Si el número es impar
+    if(numero % 2 !== 0) {
+        return console.info(`El número ${numero} es impar`);
+    } else {
+        return console.info(`El número ${numero} es par`);
+    }
+}
+
+parImpar(2);
+
 // 14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
+
+// no lo haré.
