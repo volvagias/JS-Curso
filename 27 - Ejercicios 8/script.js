11edc1b6cbc4dac2bbad7df6e060e576e83c439b
Leonel <leonel.bruzzese@gmail.com>
2024-09-20 13:08:28 -0300

    Ejercicio 27 Capitulo 9.

--- 27 - Ejercicios 8/script.js
+++ 27 - Ejercicios 8/script.js
@@ -41,3 +41,5 @@ function eliminarDuplicados(array2) {
 eliminarDuplicados(["x", 10, "x", 2, "10", 10, true, true]);
 
 // 26) Programa una función que dado un arreglo de números obtenga el promedio, pe. promedio([9,8,7,6,5,4,3,2,1,0]) devolverá 4.5.
+
+// no se que es promedio.
