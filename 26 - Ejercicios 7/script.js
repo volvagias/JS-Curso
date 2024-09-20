ac1822d35e1684b818b19319c07b3f43547c4d83
Leonel <leonel.bruzzese@gmail.com>
2024-09-20 08:57:00 -0300

    Ejercicios 7.

--- 26 - Ejercicios 7/script.js
+++ 26 - Ejercicios 7/script.js
@@ -34,3 +34,25 @@ function valoresMaximoMinimo(array2) {
 valoresMaximoMinimo([1, 4, 5, 99, -60]);
 
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
+
+function arrayDeNumerosParesImpares(array3) {
+    if(!(array3 instanceof Array)) return console.error('Debe ingresar un arreglo.');
+
+    let newArray = { 
+        pares: [],
+        impares: [] 
+    };
+
+    for (let i = 0; i < array3.length; i++) {
+        // Si el número es impar
+        if(array3[i] % 2 !== 0) {
+            newArray.impares.push(array3[i]);
+        } else {
+            newArray.pares.push(array3[i]);
+        }
+    }
+
+    return console.info(newArray);
+}
+
+arrayDeNumerosParesImpares([1,2,3,4,5,6,7,8,9,0]);
