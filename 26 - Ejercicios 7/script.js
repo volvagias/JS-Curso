b08b4cb3b6f737f9201dcb5f292bbf5717ac6f95
Leonel <leonel.bruzzese@gmail.com>
2024-09-20 07:57:13 -0300

    Ejercicio 22 Capítulo 26.

--- 26 - Ejercicios 7/script.js
+++ 26 - Ejercicios 7/script.js
@@ -19,4 +19,18 @@ function elevarAlCuadrado(array) {
 elevarAlCuadrado([1, 4, 5]);
 
 // 22) Programa una función que dado un array devuelva el número mas alto y el más bajo de dicho array, pe. miFuncion([1, 4, 5, 99, -60]) devolverá [99, -60].
+
+function valoresMaximoMinimo(array2) {
+    if(!(array2 instanceof Array)) return console.error('Debe ingresar un arreglo.');
+
+    let newArray = [];
+
+    newArray.push(Math.max(...array2)); // Es equivalente a Math.max(1, 4, 5, 99, -60). Se usa el spread operator porque el Math.max o min no acepta arrays como argumento.
+    newArray.push(Math.min(...array2));    
+
+    return console.info(newArray);
+}
+
+valoresMaximoMinimo([1, 4, 5, 99, -60]);
+
 // 23) Programa una función que dado un array de números devuelva un objeto con 2 arreglos en el primero almacena los números pares y en el segundo los impares, pe. miFuncion([1,2,3,4,5,6,7,8,9,0]) devolverá {pares: [2,4,6,8,0], impares: [1,3,5,7,9]}.
