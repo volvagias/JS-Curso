9cbe6b1dc4a2f5e3a39a9568376aec99f1083963
Leonel <leonel.bruzzese@gmail.com>
2025-01-21 09:28:48 -0300

    JSON terminado.

---
+++ 44 - JSON/datos.js
@@ -0,0 +1,12 @@
+
+{
+    "cadena": "Leonel",
+    "numero": "31",
+    "booleano": "true",
+    "arreglo": ["correr", "programar", "cocinar"],
+    "objeto": {
+        "instagram": "@volvagias",
+        "email": "leonel.bruzzese@gmail.com"
+    },
+    "nulo": "null"
+}
